122078079240948fa700af0e98326d09245078c1
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-01-16 16:19:35 -0500

    hello world program test

--- HelloWorld/src/ca/mcgill/ecse211/HelloWorld/HelloWorld.java
+++ HelloWorld/src/ca/mcgill/ecse211/HelloWorld/HelloWorld.java
@@ -1,5 +1,18 @@
 package ca.mcgill.ecse211.HelloWorld;
 
+import lejos.hardware.Button;
+
+/*
+ * Hello world program test
+ * Display "Hello World!" on EV3 and quit when any button pressed
+ */
 public class HelloWorld {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Hello World!");
+		Button.waitForAnyEvent();
+	}
+	
 	
 }
