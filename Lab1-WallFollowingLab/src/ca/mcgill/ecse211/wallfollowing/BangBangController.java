1461cf23c00654da17162ed8db21e4a64c3038af
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-01-17 19:48:06 -0500

    test sensor

--- Lab1-WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
+++ Lab1-WallFollowingLab/src/ca/mcgill/ecse211/wallfollowing/BangBangController.java
@@ -4,43 +4,55 @@ import lejos.hardware.motor.*;
 
 public class BangBangController implements UltrasonicController {
 
-  private final int bandCenter;
-  private final int bandwidth;
-  private final int motorLow;
-  private final int motorHigh;
-  private int distance;
+	private final int bandCenter;
+	private final int bandwidth;
+	private final int motorLow;
+	private final int motorHigh;
+	private int distance;
 
-  public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
-    // Default Constructor
-    this.bandCenter = bandCenter;
-    this.bandwidth = bandwidth;
-    this.motorLow = motorLow;
-    this.motorHigh = motorHigh;
-    WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
-    WallFollowingLab.rightMotor.setSpeed(motorHigh);
-    WallFollowingLab.leftMotor.forward();
-    WallFollowingLab.rightMotor.forward();
-  }
+	public BangBangController(int bandCenter, int bandwidth, int motorLow, int motorHigh) {
+		// Default Constructor
+		this.bandCenter = bandCenter;
+		this.bandwidth = bandwidth;
+		this.motorLow = 50;
+		this.motorHigh = 150;
+		WallFollowingLab.leftMotor.setSpeed(motorHigh); // Start robot moving forward
+		WallFollowingLab.rightMotor.setSpeed(motorHigh);
+		WallFollowingLab.leftMotor.forward();
+		WallFollowingLab.rightMotor.forward();
+	}
 
-  @Override
-  public void processUSData(int distance) {
-    this.distance = distance;
-    // TODO: process a movement based on the us distance passed in (BANG-BANG style)
-    
-    // if robot is too far and out of hunting region
-    if(distance > (this.bandCenter + this.bandwidth)) {
-    	// sensor is on the left
-    	// left wheel slower
-    	WallFollowingLab.leftMotor.setSpeed(motorLow);
-    } else if(distance < (this.bandCenter - this.bandwidth)) {
-    	// robot is too close to wall
-    }
-    
-    
-  }
+	@Override
+	public void processUSData(int distance) {
+		this.distance = distance;
+		// TODO: process a movement based on the us distance passed in (BANG-BANG style)
 
-  @Override
-  public int readUSDistance() {
-    return this.distance;
-  }
+		// adjustment factor 40 to distance; speed increment = 50
+
+		if(distance < 200) {
+
+			if(distance > (this.bandCenter + 40 + this.bandwidth)) {
+				// far
+				// turn left
+				WallFollowingLab.leftMotor.setSpeed(motorHigh - 50);
+				WallFollowingLab.rightMotor.setSpeed(motorHigh + 50);
+				System.out.println("far");
+
+			} else if(distance < (this.bandCenter + 40 - this.bandwidth)) {
+				// close
+				// turn right
+				WallFollowingLab.leftMotor.setSpeed(motorHigh + 50);
+				WallFollowingLab.rightMotor.setSpeed(motorHigh - 50);
+				System.out.println("close");
+			} else {
+				// keep going
+			}
+		}
+
+	}
+
+	@Override
+	public int readUSDistance() {
+		return this.distance;
+	}
 }
