82eac8dab8beba2844f9ba8c15a8c42c315f8b6c
erica <erica.depetrillo@mail.mcgill.ca>
2019-01-24 18:25:54 -0500

    odometer code version 1

---
+++ Lab2-Odometer/src/ca/mcgill/ecse211/odometer/Odometer.java
@@ -0,0 +1,150 @@
+/**
+ * This class is meant as a skeleton for the odometer class to be used.
+ * 
+ * @author Rodrigo Silva
+ * @author Dirk Dubois
+ * @author Derek Yu
+ * @author Karim El-Baba
+ * @author Michael Smith
+ */
+
+package ca.mcgill.ecse211.odometer;
+
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+public class Odometer extends OdometerData implements Runnable {
+
+  private OdometerData odoData;
+  private static Odometer odo = null; // Returned as singleton
+
+  // Motors and related variables
+  private int leftMotorTachoCount;
+  private int rightMotorTachoCount;
+  private EV3LargeRegulatedMotor leftMotor;
+  private EV3LargeRegulatedMotor rightMotor;
+
+  private final double TRACK;
+  private final double WHEEL_RAD;
+
+  private double[] position;
+
+
+  private static final long ODOMETER_PERIOD = 25; // odometer update period in ms
+
+  /**
+   * This is the default constructor of this class. It initiates all motors and variables once.It
+   * cannot be accessed externally.
+   * 
+   * @param leftMotor
+   * @param rightMotor
+   * @throws OdometerExceptions
+   */
+  private Odometer(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor,
+      final double TRACK, final double WHEEL_RAD) throws OdometerExceptions {
+    odoData = OdometerData.getOdometerData(); // Allows access to x,y,z
+                                              // manipulation methods
+    this.leftMotor = leftMotor;
+    this.rightMotor = rightMotor;
+
+    // Reset the values of x, y and z to 0
+    odoData.setXYT(0, 0, 0);
+
+    this.leftMotorTachoCount = 0;
+    this.rightMotorTachoCount = 0;
+
+    this.TRACK = TRACK;
+    this.WHEEL_RAD = WHEEL_RAD;
+
+  }
+
+  /**
+   * This method is meant to ensure only one instance of the odometer is used throughout the code.
+   * 
+   * @param leftMotor
+   * @param rightMotor
+   * @return new or existing Odometer Object
+   * @throws OdometerExceptions
+   */
+  public synchronized static Odometer getOdometer(EV3LargeRegulatedMotor leftMotor,
+      EV3LargeRegulatedMotor rightMotor, final double TRACK, final double WHEEL_RAD)
+      throws OdometerExceptions {
+    if (odo != null) { // Return existing object
+      return odo;
+    } else { // create object and return it
+      odo = new Odometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);
+      return odo;
+    }
+  }
+
+  /**
+   * This class is meant to return the existing Odometer Object. It is meant to be used only if an
+   * odometer object has been created
+   * 
+   * @return error if no previous odometer exists
+   */
+  public synchronized static Odometer getOdometer() throws OdometerExceptions {
+
+    if (odo == null) {
+      throw new OdometerExceptions("No previous Odometer exits.");
+
+    }
+    return odo;
+  }
+
+  /**
+   * This method is where the logic for the odometer will run. Use the methods provided from the
+   * OdometerData class to implement the odometer.
+   */
+  // run method (required for Thread)
+  public void run() {
+    long updateStart, updateEnd;
+
+    while (true) {
+      updateStart = System.currentTimeMillis();
+
+      int leftMotorTachoCountNew = leftMotor.getTachoCount(); //in degrees
+      int rightMotorTachoCountNew = rightMotor.getTachoCount();
+      position = odoData.getXYT();
+      
+      int leftPhi = leftMotorTachoCountNew - leftMotorTachoCount;
+      int rightPhi = rightMotorTachoCountNew - rightMotorTachoCount;
+      
+      leftMotorTachoCount = leftMotorTachoCountNew; //updates the tacho count
+      rightMotorTachoCount = rightMotorTachoCountNew; //updates the tacho count
+      
+      int leftDistance = leftPhi/2046*100; //equivalent to dividing by 20.46 (to go from degrees to distance)
+      int rightDistance = rightPhi/2046*100;
+      
+      int distance = rightDistance - leftDistance;
+      int wheelBase = 13; //im not entirely sure what wheelbase is, but im assuming its the distance between the 2 wheels aka 6.62*2 ~ 13
+      
+      double radTheta = distance/wheelBase; //in radians
+      
+      double dTheta = radTheta*180/Math.PI; //in degrees
+      
+      double displacement = (leftDistance + rightDistance)/2;
+      double dx = displacement*Math.sin(position[2] + dTheta);
+      double dy = displacement*Math.cos(position[2] + dTheta);
+
+      // TODO Calculate new robot position based on tachometer counts
+     
+      //choose robot facing right as 0 degrees, theta increases counter clockwise, just like for polar coordinates
+      
+      
+      
+      // TODO Update odometer values with new calculated values
+      odo.update(dx, dy, dTheta);
+
+      // this ensures that the odometer only runs once every period
+      updateEnd = System.currentTimeMillis();
+      if (updateEnd - updateStart < ODOMETER_PERIOD) {
+        try {
+          Thread.sleep(ODOMETER_PERIOD - (updateEnd - updateStart));
+        } catch (InterruptedException e) {
+          // there is nothing to be done
+        }
+      }
+    }
+  }
+
+}
