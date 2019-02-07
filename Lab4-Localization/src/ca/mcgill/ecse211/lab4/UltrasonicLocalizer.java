2c7ae31710ef3a51a9b89da45cc33e0e48ca0bc8
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-02-07 14:51:32 -0500

    Update UltrasonicLocalizer.java

--- Lab4-Localization/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
+++ Lab4-Localization/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -1,13 +1,12 @@
 package ca.mcgill.ecse211.lab4;
 
-//non-static imports
+// non-static imports
 import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 import ca.mcgill.ecse211.odometer.*;
-
 // static imports
 import static ca.mcgill.ecse211.lab4.Lab4.LEFT_MOTOR;
 import static ca.mcgill.ecse211.lab4.Lab4.RIGHT_MOTOR;
@@ -21,68 +20,146 @@ public class UltrasonicLocalizer extends Thread {
   private static final double THRESHOLD = 40;
   private static final double MARGIN = 5;
   private static final double TURN_CIRCLE = 360.0;
-  
+
+  private static final int FALLING_EDGE = Button.ID_LEFT;
+  private static final int RISING_EDGE = Button.ID_RIGHT;
+
   private int type;
-  
+
   private Odometer odometer;
   private double[] odoData;
-  
+
   private SampleProvider usDistance;
   private float[] usData;
-  
-  public UltrasonicLocalizer(int buttonChoice, SampleProvider usDistance, float[] usData) throws OdometerExceptions {
-    this.type = buttonChoice;
+
+  public UltrasonicLocalizer(int buttonChoice, SampleProvider usDistance, float[] usData)
+      throws OdometerExceptions {
     this.odometer = Odometer.getOdometer();
     this.usData = usData;
     this.usDistance = usDistance;
+    this.type = buttonChoice;
     LEFT_MOTOR.setAcceleration(500);
     RIGHT_MOTOR.setAcceleration(500);
   }
-  
+
   public void run() {
-    
+
     LEFT_MOTOR.rotate(convertAngle(WHEEL_RAD, TRACK, TURN_CIRCLE), true);
     RIGHT_MOTOR.rotate(-convertAngle(WHEEL_RAD, TRACK, TURN_CIRCLE), true);
-    
-    if(type == Button.ID_LEFT) {
+
+    if (type == FALLING_EDGE) {
       // falling edge
       fallingEdge();
-    } else if(type == Button.ID_RIGHT) {
+    } else if (type == RISING_EDGE) {
       // rising edge
       risingEdge();
     }
-    
+
   }
-  
+
+  /**
+   * 
+   */
   private void fallingEdge() {
-    
+
     double T1, T2, alpha, T3, T4, beta;
-   
-    while(true) {
+
+    while (true) {
       double distance = medianFilter();
-      
-      if(distance < THRESHOLD + MARGIN) {
+
+      // falling edge
+      if (distance < THRESHOLD + MARGIN) {
         T1 = odometer.getXYT()[2];
-        while(true) {
+        while (true) {
           distance = medianFilter();
-          if(distance < THRESHOLD - MARGIN) {
-            T2 = odometer.getXYT()[2]; 
+          if (distance < THRESHOLD - MARGIN) {
+            T2 = odometer.getXYT()[2];
             break;
           }
         }
         break;
       }
-      
-      
     }
-    
-    
-    
-    
+
+    alpha = (T1 + T2) / 2;
+
+    while (true) {
+      double distance = medianFilter();
+
+      // rising edge
+      if (distance > THRESHOLD - MARGIN) {
+        T3 = odometer.getXYT()[2];
+        while (true) {
+          distance = medianFilter();
+          if (distance > THRESHOLD + MARGIN) {
+            T4 = odometer.getXYT()[2];
+            break;
+          }
+        }
+        break;
+      }
+    }
+
+    beta = (T3 + T4) / 2;
+
+    REcorrect(alpha, beta);
   }
-  
+
+  /**
+   * 
+   */
   private void risingEdge() {
-    
+
+    double T1, T2, alpha, T3, T4, beta;
+
+    while (true) {
+      double distance = medianFilter();
+
+      // falling edge
+      if (distance > THRESHOLD - MARGIN) {
+        T1 = odometer.getXYT()[2];
+        while (true) {
+          distance = medianFilter();
+          if (distance > THRESHOLD + MARGIN) {
+            T2 = odometer.getXYT()[2];
+            break;
+          }
+        }
+        break;
+      }
+    }
+
+    alpha = (T1 + T2) / 2;
+
+    while (true) {
+      double distance = medianFilter();
+
+      // rising edge
+      if (distance < THRESHOLD + MARGIN) {
+        T3 = odometer.getXYT()[2];
+        while (true) {
+          distance = medianFilter();
+          if (distance < THRESHOLD - MARGIN) {
+            T4 = odometer.getXYT()[2];
+            break;
+          }
+        }
+        break;
+      }
+    }
+
+    beta = (T3 + T4) / 2;
+
+    FEcorrect(alpha, beta);
+
+  }
+
+  private double FEcorrect(double alpha, double beta) {
+    return 255.0 - (alpha + beta) / 2.0;
+  }
+
+  private double REcorrect(double alpha, double beta) {
+    return 45.0 - (alpha + beta) / 2.0;
   }
   
   /**
@@ -95,14 +172,14 @@ public class UltrasonicLocalizer extends Thread {
    */
   private double medianFilter() {
     double[] arr = new double[5];
-    for(int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++) {
       this.usDistance.fetchSample(usData, 0);
-      arr[i] = usData[0] * 100.0; 
+      arr[i] = usData[0] * 100.0;
     }
     Arrays.sort(arr);
-    return arr[2]; 
+    return arr[2];
   }
-  
+
   /**
    * This is a static method allows the conversion of a distance to the total rotation of each wheel
    * need to cover that distance.
