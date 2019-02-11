23259a4ae1b80df88c46789a276e06194e83639f
Raymond H. Yang <raymond.yang@mail.mcgill.ca>
2019-02-11 01:25:47 -0500

    lab 4 javadoc

--- Lab4-Localization/src/ca/mcgill/ecse211/lab4/Lab4.java
+++ Lab4-Localization/src/ca/mcgill/ecse211/lab4/Lab4.java
@@ -23,12 +23,16 @@ import ca.mcgill.ecse211.odometer.*;
  * @author Erica De Petrillo
  */
 public class Lab4 {
-  
+
+  // -----------------------------------------------------------------------------
+  // Public Constants
+  // -----------------------------------------------------------------------------
+
   /**
    * The radius (in cm) of the left/right wheels of the EV3 robot.
    */
   public static final double WHEEL_RAD = 2.15;
-  
+
   /**
    * The width (in cm) of the robot measured from the center of the left wheel to the center of the
    * right wheel
@@ -68,58 +72,88 @@ public class Lab4 {
   // Main Method
   // -----------------------------------------------------------------------------
 
+  /**
+   * In this class, sensors are initiated and display is setup. User is given the option to choose a
+   * mode of ultrasonic sensor localization. Program responses to user choices are specified here.
+   * 
+   * @param args - None
+   * @throws OdometerExceptions - Thrown by odometer; only one instance of Odometer is allowed to
+   *         exist
+   */
   public static void main(String[] args) throws OdometerExceptions {
 
-    int buttonChoice; // variable to record button clicked by user
+    // Variable to record button clicked by user
+    int buttonChoice;
 
+    // Initiates an odometer instance, used for recording and updating vehicle position data
     Odometer odometer = Odometer.getOdometer(LEFT_MOTOR, RIGHT_MOTOR, TRACK, WHEEL_RAD);
 
+    // The LCD display of the robot, allows control of information to be displayed
     Display odometryDisplay = new Display(LCD);
 
-    // US sensor initialization
+    // Ultrasonic sensor initialization
+    // 1. get sensor instance to set operating mode
+    // 2. set operating mode and setup uniform data reader
+    // 3. buffer to store data read from sensor
+    // The 3 steps correspond to the three lines of code setting up the sensor
     @SuppressWarnings("resource")
     SensorModes usSensor = new EV3UltrasonicSensor(US_PORT);
     SampleProvider usDistance = usSensor.getMode("Distance");
     float[] usData = new float[usDistance.sampleSize()];
 
-    // CS sensor initialization
+    // Color sensor initialization
+    // Similar process to Ultrasonic sensor initialization above
     SensorModes csSensor = new EV3ColorSensor(CS_PORT);
     SampleProvider cs = csSensor.getMode("Red");
     float[] csData = new float[cs.sampleSize()];
 
+    // Set up LCD display on EV3 brick
+    // Option to choose mode for ultrasonic sensor localization
+    // This prompt is repeated until user presses right, left, or esc button
     do {
+      // Clear LCD display
       LCD.clear();
       LCD.drawString("< Left  |  Right >", 0, 0);
       LCD.drawString("        |         ", 0, 1);
       LCD.drawString("Falling |  Rising ", 0, 2);
       LCD.drawString(" Edge   |   Edge  ", 0, 3);
+      // Record button choice
       buttonChoice = Button.waitForAnyPress();
+      // Condition to repeat prompt
     } while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT
         && buttonChoice != Button.ID_ESCAPE);
 
+    // Left or right button: falling edge or rising edge
+    // esc button: exits program
     if (buttonChoice == Button.ID_LEFT || buttonChoice == Button.ID_RIGHT) {
-      // Falling Edge or Rising Edge
 
+      // Set up ultrasonic sensor localization
       UltrasonicLocalizer usloc =
           new UltrasonicLocalizer(buttonChoice, usDistance, usData, odometer);
 
+      // Create threads of odometer, display, and ultrasonic sensor localizer program
       Thread odoThread = new Thread(odometer);
       Thread odoDisplayThread = new Thread(odometryDisplay);
       Thread uslocThread = new Thread(usloc);
 
+      // Start threads
       odoThread.start();
       odoDisplayThread.start();
       uslocThread.start();
 
-      if (Button.waitForAnyPress() == Button.ID_ESCAPE) { // wait for user confirmation
+      // Allows verification of ultrasonic localization before starting light localization
+      if (Button.waitForAnyPress() == Button.ID_ESCAPE) {
+        // Exits system if esc button pressed
         System.exit(0);
       } else {
+        // Light localizer started
         LightLocalizer lightloc = new LightLocalizer(cs, csData, odometer);
         Thread lightlocThread = new Thread(lightloc);
         lightlocThread.start();
       }
 
     } else {
+      // exits program
       System.exit(0);
     }
 
